a0174b6019eeeda6f2af86b938fdc0695fd52249
unknown <krngd2@gmail.com>
2018-08-25 21:34:07 +0530

    3-sum

--- 3-sum.py
+++ 3-sum.py
@@ -10,37 +10,34 @@
 # and target = 1.
 
 
-import numpy as np
+import numpy as np 
+import sys
+class Solution:
+    def threeSumClosest(self, arr, target):
+        arr.sort()
+        length = len(arr) - 1
+        self.min = sys.maxsize 
+        self.results = None
+        for i in range(length):
+            j = i + 1 
+            k = length
+            while j < k :
+                sum =  arr[i] + arr[j] + arr[k] 
+                print(arr[i], arr[j], arr[k], '=', sum )
+                clos =  np.abs(sum-target)
+                if clos == 0:
+                    return sum
+                if clos < self.min:
+                    self.min = clos
+                    self.results = sum
+                if sum <=  target:
+                    j+=1
+                else:
+                    k-=1
+        return self.results
+  
 
-s =[-1, 2, 1, -4]   # given Array
-target = 1          # given target
-results = []        # results of sum of 3 numbers in array 
-
-# raise exception if array length is less than 3
-if len(s) < 3:
-    raise Exception('Array Must be length of more than 3')
-
-# to add 3 numbers in an array loop array 3 times
-for i in range(len(s)):  
-    for j in range(len(s)):
-        if not s[i] == s[j]: # preventing adding self
-            for k in range(len(s)):
-                if not s[i] == s[k] and not s[j] == s[k]  :     # preventing adding to self
-                    print((s[i],s[j], s[k]),'=',s[i] + s[j] + s[k] )
-                    addedResult = s[i] + s[j] + s[k]            
-                    if addedResult not in results:              # preventing duplicate entries
-                        results.append(addedResult)             # adding to the results array
-
-
-print(results) 
-
-# getting the nearest value to target from the results array using numpy
-# loop through the results and get absolute values when subtracted from the target 
-# and append to another array to preserve its index
-absoluteValues= [] 
-absoluteValues.append([np.abs(i - target) for i in results])  
-
-# get the index of the minimum value of absoluteValues array 
-# and get the value of the same index from results array
-print(results[absoluteValues.index(min(absoluteValues))])       
-# result is 2 
+sum = Solution()
+S = [-1, -2, 1, -4, 8 , 2]
+target = -3
+print(sum.threeSumClosest(S,target))
